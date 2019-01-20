41bb3884935b5b0833149d81115f38bb6bed6ca4
Atanas Krastev <at_krastev@yahoo.com>
2019-01-20 16:38:05 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumberEquals
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Въведете стойност а = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Въведете стойност b = ");
+            int b = int.Parse(Console.ReadLine());
+        
+            int sum = a + b;
+            Console.WriteLine("Sum = {0}", sum);
+            
+            if (sum > 10)
+            {
+                Console.WriteLine(" The number {0} is true", sum);
+                Console.WriteLine(DateTime.Now);
+            }
+            else if (sum < 10)
+            {
+                Console.WriteLine("The number {0} is false", sum);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is error", sum);
+            } 
+        }
+    }
+}
